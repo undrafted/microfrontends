ad640ef1dd8a754693855a38aa6f737db4d718fc
undrafted <8jermiedomingo@gmail.com>
2020-12-25 15:01:19 +0100

    decoupling from hard coded DOM ids

--- hello/cart/src/bootstrap.js
+++ hello/cart/src/bootstrap.js
@@ -1,5 +1,19 @@
 import faker from "faker";
 
-const cartText = `<div>You have ${faker.random.number()} items in your cart!</div>`;
+const mount = (el) => {
+  const cartText = `<div>You have ${faker.random.number()} items in your cart!</div>`;
 
-document.querySelector("#dev-cart").innerHTML = cartText;
+  el.innerHTML = cartText;
+};
+
+// mount if MFE is used in isolation/development (not in a host)
+if (process.env.NODE_ENV === "development") {
+  // assuming id #dev-cart is only used in this project for isolated development
+  const el = document.querySelector("#dev-cart");
+
+  if (el) {
+    mount(el);
+  }
+}
+
+export { mount };
--- hello/products/src/bootstrap.js
+++ hello/products/src/bootstrap.js
@@ -1,9 +1,23 @@
 import faker from "faker";
 
-let products = "";
+const mount = (el) => {
+  let products = "";
 
-for (let i = 0; i < 3; i++) {
-  products += `<div>${faker.commerce.productName()}</div>`;
+  for (let i = 0; i < 3; i++) {
+    products += `<div>${faker.commerce.productName()}</div>`;
+  }
+
+  el.innerHTML = products;
+};
+
+// mount if MFE is used in isolation/development (not in a host)
+if (process.env.NODE_ENV === "development") {
+  // assuming id #dev-products is only used in this project for isolated development
+  const el = document.querySelector("#dev-products");
+
+  if (el) {
+    mount(el);
+  }
 }
 
-document.querySelector("#dev-products").innerHTML = products;
+export { mount };
